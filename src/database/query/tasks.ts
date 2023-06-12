900eb55f6846e094595c3ff119980d97c7d4b91d
Eng-Mohammed-Sbeata <majd86787@gmail.com>
2023-06-12 19:49:55 +0300

    fex: delete id

--- src/database/query/tasks.ts
+++ src/database/query/tasks.ts
@@ -2,9 +2,8 @@ import connection from '../config/connection';
 import { Query } from '../../interfaces';
 
 const addTaskQuery = ({
-  id, title, description, priorityId, projectId, sectionId, dueDate,
+  title, description, priorityId, projectId, sectionId, dueDate,
 }: {
-    id: number,
     title: string,
     description: string,
     priorityId: number,
@@ -16,7 +15,7 @@ const addTaskQuery = ({
     text: `INSERT INTO tasks(title, description, priority_id, project_id, section_id, due_date)
             VALUES($1, $2, $3, $4, $5, $6)
             RETURNING title, description, priority_id, project_id, section_id, due_date`,
-    values: [id, title, description, priorityId, projectId, sectionId, dueDate],
+    values: [title, description, priorityId, projectId, sectionId, dueDate],
   };
   return connection.query(sql);
 }
