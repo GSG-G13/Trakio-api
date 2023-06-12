f5d9d252b0dc685ba5e03f9767d4d1d78864e5d3
Eng-Mohammed-Sbeata <majd86787@gmail.com>
2023-06-12 19:42:32 +0300

    refact: update query

--- src/database/query/tasks.ts
+++ src/database/query/tasks.ts
@@ -13,8 +13,8 @@ const addTaskQuery = ({
     dueDate: string,
 }) => {
   const sql: Query = {
-    text: `INSERT INTO tasks(id, title, description, priority_id, project_id, section_id, due_date)
-            VALUES($1, $2, $3, $4, $5, $6, $7)
+    text: `INSERT INTO tasks(title, description, priority_id, project_id, section_id, due_date)
+            VALUES($1, $2, $3, $4, $5, $6)
             RETURNING title, description, priority_id, project_id, section_id, due_date`,
     values: [id, title, description, priorityId, projectId, sectionId, dueDate],
   };
