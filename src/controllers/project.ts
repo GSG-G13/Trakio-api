00c06db668ea9640eedfccea2d306807d6a9d1e2
Amal-Mousa <amalalsattari@gmail.com>
2023-06-13 16:11:17 +0300

    feat: add project controller without comments

--- src/controllers/project.ts
+++ src/controllers/project.ts
@@ -5,33 +5,21 @@ import { TokenRequest, ProjectData, ProjectDetailsInterface, projectDataDetails 
 import { CustomError } from '../helper';
 import { projectSchema } from '../validation';
 
-const addProjectController = (req: TokenRequest, _res: Response, next: NextFunction) => {
+const addProjectController = (req: TokenRequest, res: Response, next: NextFunction) => {
   const userId = req.userData?.id;
-  let projectInfo: projectDataDetails = {
-    title: '',
-    description: '',
-    id: 0,
-    created_at: Date.now(),
-  };
+
   const { title, description } = req.body as ProjectData;
 
   projectSchema.validateAsync({ title, description } as ProjectData)
     .then((data: ProjectData) => addProjectQuery(data.title, data.description))
     .then((data: QueryResult) => {
-      projectInfo = { title: 'fff', description: 'fff' };
       addProjectUserQuery(+userId!, data.rows[0].id, 1)
     })
-    // .then(() => {
-    //   _res.status(201).json({
-    //     message: 'New Project added Successfully',
-    //     data: {
-    //       id: data.rows[0].id,
-    //       title: data.rows[0].title,
-    //       description: data.rows[0].description,
-    //       created_at: data.rows[0].created_at,
-    //     },
-    //   });
-    // })
+    .then(() => {
+      res.status(201).json({
+        message: 'New Project added Successfully',
+      })
+    })
     .catch(() => next(new CustomError(500, 'server Error')));
 }
 
