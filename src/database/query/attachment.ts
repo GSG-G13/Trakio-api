a2ca7101c5598f79c48b7f5069582a121fca3284
Eng-Mohammed-Sbeata <majd86787@gmail.com>
2023-06-14 20:40:40 +0300

    feat: create add attachments query

---
+++ src/database/query/attachment.ts
@@ -0,0 +1,26 @@
+import connection from '../config';
+import { Query } from '../../interfaces';
+
+const addAttachmentQuery = ({
+  attachS3, userId, taskId,
+}: {
+    attachS3: Text,
+    userId: number,
+    taskId: number,
+        }) => {
+  const sql: Query = {
+    text: `INSERT INTO attachments (attach_s3, user_id, task_id)
+            VALUES ($1, $2, $3)
+            RETURNING attach_s3, user_id, task_id`,
+    values: [attachS3, userId, taskId],
+  }
+  return connection.query(sql);
+}
+const getAttachmentQuery = (userId: number, taskId: number) => {
+  const sql: Query = {
+    text: 'SELECT * FROM attachments WHERE user_id = $1 and task_id = $2',
+    values: [userId, taskId],
+  }
+  return connection.query(sql);
+};
+export { getAttachmentQuery, addAttachmentQuery };
