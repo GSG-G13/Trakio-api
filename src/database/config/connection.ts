de75ae161e20f28debbdc9b2bec021835d7b944a
KhaledToha <khaled.f.toha@gmail.com>
2023-06-25 16:01:00 +0300

    refactor: refactoring the back end

--- src/database/config/connection.ts
+++ src/database/config/connection.ts
@@ -3,10 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// const { DB_URL } = process.env;
-
 const db: string = process.env.NODE_ENV === 'production' ? process.env.DB_URL! : process.env.DEV_DB_URL!
-console.log(db);
 
 const options: PoolConfig = {
   connectionString: db,
