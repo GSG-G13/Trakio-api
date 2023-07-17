144b141d645871209f3320dcc4b50f43ec0603eb
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2023-07-18 01:03:08 +0300

    fix: update origin link

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,25 @@
+/* eslint-disable no-unused-expressions */
 import express, { Express } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 import router from './routers';
 
-dotenv.config()
+dotenv.config();
 
-const app:Express = express();
+const app: Express = express();
 
 app.use([
   express.json(),
   express.urlencoded({ extended: false }),
-  cors(),
+  cors({
+    origin: process.env.BASE_URL,
+    credentials: true,
+  }),
   cookieParser(),
 ]);
 
-// eslint-disable-next-line no-unused-expressions
 process.env.Node_ENV === 'development' && app.use(morgan('dev'));
 app.use('/api', router);
 
